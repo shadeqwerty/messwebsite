a776df1af9bab798ab4e39385989779aeba8d218
Shankar Ram <shankarramnr@gmail.com>
2024-05-01 12:42:19 +0530

    Relative path

--- Processor_Codes/maincode.py
+++ Processor_Codes/maincode.py
@@ -2,6 +2,7 @@ import pandas as pd
 from .Processor import *
 from .html_maker import *
 from ..Front_end.Html_front import *
+import os
 def hmtl_synth():
     current_time_ist, day,session =  get_current_time_session_day()
     Next_menu_item = get_menu_items(day, session)
@@ -14,5 +15,8 @@ def hmtl_synth():
 
     #html_made = html_made.replace('&lt;br&gt;', '<br>')
     html_made = generate_html(now_time, Next_menu_item, latest_updated_menu, access_time ,day,session)
-    with open('messwebsite/templates/index2.html', 'w',encoding='utf-8') as f:
+    current_file_dir = os.path.dirname(os.path.abspath(__file__))
+    template_path = os.path.join(current_file_dir, '../templates/index2.html')
+    
+    with open(template_path, 'w',encoding='utf-8') as f:
         f.write(html_made)
